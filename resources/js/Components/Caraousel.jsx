07c67b534c44bbe971a0d5d30a6daa8e15909d90
avikpl1911 <avikpl1911@gmail.com>
2023-10-08 20:13:01 +0530

    first commit

---
+++ resources/js/Components/Caraousel.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+ 
+function Caraousel() {
+    return (
+      <div className='flex justify-center'>
+
+<div className="w-4/5  carousel rounded-box ">
+  <div className="carousel-item w-full">
+    <img src="https://flowbite.com/docs/images/blog/image-1.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+  </div> 
+  <div className="carousel-item w-full">
+    <img src="https://flowbite.com/docs/images/blog/image-1.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+  </div> 
+  <div className="carousel-item w-full">
+    <img src="https://flowbite.com/docs/images/blog/image-1.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+  </div> 
+  <div className="carousel-item w-full">
+    <img src="https://flowbite.com/docs/images/blog/image-1.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+  </div> 
+  <div className="carousel-item w-full">
+    <img src="https://flowbite.com/docs/images/blog/image-1.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+  </div> 
+  <div className="carousel-item w-full">
+    <img src="https://flowbite.com/docs/images/blog/image-1.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+  </div> 
+  <div className="carousel-item w-full">
+    <img src="https://flowbite.com/docs/images/blog/image-1.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
+  </div>
+</div>
+      </div>
+        
+
+    )
+}
+
+export default Caraousel
